e57d6ec27bd342e8047b31e6d31d84559b231551
Jesimiel <jesimielbarbosa@gmail.com>
2024-10-25 16:18:19 -0300

    criada as paginas de login e cadastro

---
+++ src/app/auth/sing-up-in/sing-up-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-up-in',
+  templateUrl: './sing-up-in.component.html',
+  styleUrls: ['./sing-up-in.component.css']
+})
+export class SingUpInComponent {
+
+}
