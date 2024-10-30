c08fee2b0de8c9007811fa28f58b8b90e6167d05
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2024-10-30 10:08:58 -0300

    Alteração
    Alteração header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc-sso-login';
-  userLogged: boolean = false
+  userLogged: boolean = true
 
   constructor(){
 
