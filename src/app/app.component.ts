0c9e6bb81db7d075c09f91540b4d743603a97e07
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2024-10-30 17:18:49 -0300

    Alteraçãoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'poc-sso-login';
-  userLogged: boolean = true
 
-  constructor(){
+  userLooged: boolean = false
+
+  constructor(private router: Router){
 
   }
+
+  ngOnInit(): void {
+    if(!this.userLooged){
+      this.router.navigateByUrl('/login')
+    }
+  }
   
+  getLogged(logged: boolean){
+    this.userLooged = logged
+  }
 
-
+  subscribeLogin(subscribe: any){
+    subscribe.logged.subscribe((res: any) =>{
+      this.getLogged(res)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSsoInComponent } from './auth/login-sso-in/login-sso-in.component';
 import { SingUpInComponent } from './auth/sing-up-in/sing-up-in.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginSsoInComponent,
-    SingUpInComponent
+    SingUpInComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
