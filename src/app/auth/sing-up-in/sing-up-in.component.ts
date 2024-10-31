ac10fbdeb6582361ee13b7e7cf3351a78daf5efb
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2024-10-31 10:15:22 -0300

    Adicionado Table

--- src/app/auth/sing-up-in/sing-up-in.component.ts
+++ src/app/auth/sing-up-in/sing-up-in.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { System } from '../../shared/models/system.model';
 
 @Component({
   selector: 'app-sing-up-in',
   templateUrl: './sing-up-in.component.html',
   styleUrls: ['./sing-up-in.component.css']
 })
-export class SingUpInComponent {
+export class SingUpInComponent implements OnInit {
+
+  systems: System[] = []
+  header: string[] = []
+
+  constructor() {
+    this.header = ['Nome Responsável', 'Validade Token', 'Número', 'Email Responsável', 'Ações']
+    this.systems = [
+      {name: 'Test1', validToken: new Date(), number: '727272733', emailUser: 'test@gmail.com'}
+    ]
+  }
+
+  ngOnInit(): void {
+ 
+  }
 
 }
