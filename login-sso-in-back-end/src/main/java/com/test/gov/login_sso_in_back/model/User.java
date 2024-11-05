303a88dfab92457f35c8e4022fde4b3846571936
Jesimiel <jesimielbarbosa@gmail.com>
2024-11-05 11:27:45 -0300

    adicionado o backend e ajuste do scroll da tela de login

---
+++ login-sso-in-back-end/src/main/java/com/test/gov/login_sso_in_back/model/User.java
@@ -0,0 +1,58 @@
+package com.test.gov.login_sso_in_back.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String cpf;
+    private String password;
+    private String roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
